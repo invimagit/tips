c3d04232809d6bdf20aafa74f58d2d696eb3d23b
Miguel Mariano <mmariano@soaint.com>
2023-01-11 17:37:19 -0500

    Accordeon Equipos Participacion

--- public/wp-content/themes/tips-digitales/src/PostTypes/Secciones/views/content-accordeon.php
+++ public/wp-content/themes/tips-digitales/src/PostTypes/Secciones/views/content-accordeon.php
@@ -1,26 +1,26 @@
-          <div class="col-md-4 col-12 accordion px-md-5 px-2" id="accordion_<?php echo $args['ID']; ?>">
-            <div class="accordion-item p-0">
-              <div class="accordion-header datos-curso-accordion" id="">
-                <button class="accordion-button-participantes accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $args['ID']; ?>" aria-expanded="false" aria-controls="collapse<?php echo $args['ID']; ?>">
-                  <?php echo get_sub_field('titulo'); ?>
-                </button>
-              </div>
-              <div class="accordion-collapse-cursos">
-                <div id="collapse<?php echo $args['ID']; ?>" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordion_<?php echo $args['ID']; ?>" style="">
-                  <div class="accordion-body accordion-descripcion-cursos mt-3 mb-2 px-2">
+<div class="col-lg-6 col-xxl-3 col-12 pb-3 accordion" id="accordion_<?php echo $args['ID']; ?>">
+  <div class="accordion-item p-0">
+    <div class="accordion-header datos-curso-accordion" id="">
+      <button class="accordion-button-participantes accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $args['ID']; ?>" aria-expanded="false" aria-controls="collapse<?php echo $args['ID']; ?>">
+        <?php echo get_sub_field('nombre_del_equipo'); ?>
+      </button>
+    </div>
+    <div class="accordion-collapse-cursos">
+      <div id="collapse<?php echo $args['ID']; ?>" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordion_<?php echo $args['ID']; ?>" style="">
+        <div class="accordion-body accordion-descripcion-cursos mt-3 mb-2 px-2">
 					<?php
-					    if( have_rows('participantes') ):
-							while ( have_rows('participantes') ) : the_row();
+					    if( have_rows('dependencias') ):
+  							while ( have_rows('dependencias') ) : the_row();
 					?>
-			                    <button type="button" class="list-group-item list-group-item-action">
-			                    	<?php echo get_sub_field('nombre'); ?>
-			                    </button>
-				    <?php
-							endwhile;
-						endif;
+                  <button type="button" class="list-group-item list-group-item-action">
+                  	<?php echo get_sub_field('nombre_de_la_dependencia'); ?>
+                  </button>
+				  <?php
+  							endwhile;
+  						endif;
 					?>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
--- public/wp-content/themes/tips-digitales/src/PostTypes/Secciones/views/content-equipo-participacion.php
+++ public/wp-content/themes/tips-digitales/src/PostTypes/Secciones/views/content-equipo-participacion.php
@@ -2,22 +2,20 @@
   $contBloques = 1;
 ?>
 <div class="container-fluid">
-  <div class="row m-2">
-    <div class="col-12">
-      <div class="col-12 col-sm-12 col-md-12 col-lg-12 mb-3">
-        <div class="col-12 mb-3 px-2">
-          <?php
-            if( have_rows('equipos', get_the_ID()) ):
-              $cont = 1;
-              while ( have_rows('equipos', get_the_ID()) ) : the_row();
-                get_template_part('src/PostTypes/Secciones/views/content', 'accordeon',array('ID' => $cont) );
-                $cont++;
-              endwhile;
-            else :
-                // no layouts found
-            endif;
-          ?>
-        </div>
+  <div class="row mx-3 my-2">
+    <div class="col-12 col-sm-12 col-md-12 col-lg-12 mb-3">
+      <div class="row mb-3 px-md-5 px-4 pb-3">
+        <?php
+          if( have_rows('equipos', get_the_ID()) ):
+            $cont = 1;
+            while ( have_rows('equipos', get_the_ID()) ) : the_row();
+              get_template_part('src/PostTypes/Secciones/views/content', 'accordeon',array('ID' => $cont) );
+              $cont++;
+            endwhile;
+          else :
+              // no layouts found
+          endif;
+        ?>
       </div>
     </div>
   </div>
